7eab55fe18caeb348e11601083847863d17b7ae0
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-20 23:09:30 +0300

    Added 6 triangles

--- EIT_Triangles/View/Home.swift
+++ EIT_Triangles/View/Home.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct Home: View {
     var body: some View {
-        VStack {
-            
+        VStack() {
+            HStack (alignment: .center, spacing: 0) {
+                Triangle()
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+                    
+                ReversedTriangle()
+                    .fill(.cyan)
+                    .frame(width: 100, height: 100)
+                Triangle()
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+            }
+            HStack(alignment: .center, spacing: 0) {
+                ReversedTriangle()
+                    .fill(.cyan)
+                    .frame(width: 100, height: 100, alignment: .center)
+                Triangle()
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+                ReversedTriangle()
+                        .fill(.cyan)
+                        .frame(width: 100, height: 100)
+                
+            }
         }
     }
 }
@@ -20,3 +44,31 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct ReversedTriangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minX))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        
+        return path
+    }
+}
+
+
