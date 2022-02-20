cf7bb4c381744dde630cb503cef0ab18544fab7c
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-20 21:50:31 +0300

    Initial Commit

---
+++ EIT_Triangles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EIT_Triangles
+//
+//  Created by Danil Peregorodiev on 20.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EIT_Triangles/EIT_TrianglesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EIT_TrianglesApp.swift
+//  EIT_Triangles
+//
+//  Created by Danil Peregorodiev on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct EIT_TrianglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
