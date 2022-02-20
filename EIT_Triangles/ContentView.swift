7c23128d0585253dcbd01bdef679a304a4c5428f
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-02-20 21:55:50 +0300

    Start project

--- EIT_Triangles/ContentView.swift
+++ EIT_Triangles/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("Triangle")
+                    .font(.callout)
+                    .foregroundColor(.black)
+                    .bold()
+                
+            }
+        }
     }
 }
 
