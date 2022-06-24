db880a436f07cdda8efa06252dabdcc697e5e4b3
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-06-24 22:28:58 +0300

    STARTED DOCTORS DATAMODEL

--- EIT_Triangles/Model/NewsModel.swift
+++ EIT_Triangles/Model/NewsModel.swift
@@ -7,10 +7,12 @@
 
 import SwiftUI
 
-struct News: Identifiable {
+struct NewsStruct: Identifiable {
     var id = UUID().uuidString
     var title: String
     var text: String
     var description: String
     var publishDate: Date
 }
+
+// рассматриваем варианты с базой данных или вызовой внешнего API
