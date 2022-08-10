0bd6bddcfe0ca33bedaf4e1e0451d2c618f1cd8e
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-08-10 16:20:18 +0300

    Added DataController for Patients database

---
+++ EIT_Triangles/DataModel/DataController.swift
@@ -0,0 +1,50 @@
+//
+//  DataController.swift
+//  EIT_Triangles
+//
+//  Created by Danil Peregorodiev on 09.08.2022.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Patients")
+    
+    init() {
+        container.loadPersistentStores { desc, error in
+            if let error = error {
+                print("Failed to load the data: \(error.localizedDescription)")
+            }
+        }
+    }
+    func save(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+            print("Success!")
+        } catch {
+            print("Failed to save the data")
+        }
+    }
+    func addPatient(fullname: String, age: Int16, diagnosis: String, eitlink: String, context: NSManagedObjectContext) {
+        let patient = Patients(context: context)
+        
+        patient.id = UUID()
+        patient.age = age
+        patient.fullname = fullname
+        patient.diangosis = diagnosis
+        patient.eitlink = eitlink
+        
+        save(context: context)
+    }
+    
+    func editPatient(patient: Patients, fullname: String, age: Int16, diagnosis: String, eitlink: String, context: NSManagedObjectContext) {
+        
+        patient.age = age
+        patient.fullname = fullname
+        patient.diangosis = diagnosis
+        patient.eitlink = eitlink
+        
+        save(context: context)
+    }
+}
